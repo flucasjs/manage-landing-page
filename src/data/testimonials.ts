9516179d9089dca39b049162ae6c689633fff42a
unknown <flucasjs@gmail.com>
2024-02-23 05:18:18 -0800

    Add testimonials section data

---
+++ src/data/testimonials.ts
@@ -0,0 +1,33 @@
+import avatarAli from '@/assets/avatars/avatar-ali.png';
+import avatarAnisha from '@/assets/avatars/avatar-anisha.png';
+import avatarRichard from '@/assets/avatars/avatar-richard.png';
+import avatarShanai from '@/assets/avatars/avatar-shanai.png';
+
+const testimonials = [
+  {
+    avatar: avatarAnisha,
+    name: 'Anisha Li',
+    comment:
+      '“Manage has supercharged our team’s workflow. The ability to maintain visibility on larger milestones at all times keeps everyone motivated.”',
+  },
+  {
+    avatar: avatarAli,
+    name: 'Ali Bravo',
+    comment:
+      '“We have been able to cancel so many other subscriptions since using Manage. There is no more cross-channel confusion and everyone is much more focused.”',
+  },
+  {
+    avatar: avatarRichard,
+    name: 'Richard Watts',
+    comment:
+      '“Manage allows us to provide structure and process. It keeps us organized and focused. I can’t stop recommending them to everyone I talk to!”',
+  },
+  {
+    avatar: avatarShanai,
+    name: 'Shanai Gough',
+    comment:
+      '“Their software allows us to track, manage and collaborate on our projects from anywhere. It keeps the whole team in-sync without being intrusive.”',
+  },
+];
+
+export default testimonials;
