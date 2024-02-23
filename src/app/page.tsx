c9df96ca01e827375103a5b85f06d29ae59bf433
unknown <flucasjs@gmail.com>
2024-02-23 05:22:53 -0800

    Add about and carousel components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,16 @@ import React from 'react';
 import Header from '@/components/Header';
 import Hero from '@/components/Hero';
 import Background from '@/components/Background';
-
+import About from '@/components/About';
+import Carousel from '@/components/Carousel';
 export default function Home() {
   return (
     <div className="relative w-full overflow-hidden">
       <Background />
       <Header />
       <Hero />
+      <About />
+      <Carousel />
     </div>
   );
 }
