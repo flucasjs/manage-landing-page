97c65293d76048e82bd9ab2bd481f040ce7170c7
unknown <flucasjs@gmail.com>
2024-02-24 02:20:47 -0800

    Update props

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { ButtonHTMLAttributes, DetailedHTMLProps } from 'react';
+import { cn } from '@/util';
 
-export default function Button({children}: React.PropsWithChildren) {
+interface ButtonProps extends DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> {
+  children: React.ReactNode;
+  className?: string;
+}
+export default function Button({ className, children, ...props}: ButtonProps) {
   return (
     <button
-      type="button"
-      className="whitespace-nowrap rounded-[22px] bg-[#F3603C] px-8 py-[0.8125rem] text-[0.8125rem] font-bold leading-[1.4615] tracking-normal text-[#FCF6F5] shadow-[0_15px_15px_-15px_rgba(255,159,142,1)] hover:bg-[#F98F75] hover:text-[#FCF6F5]"
+      type={props.type ?? "button"}
+      className={
+        cn(
+          "whitespace-nowrap rounded-[22px] bg-[#F3603C] px-8 py-[0.8125rem] text-[0.8125rem] font-bold leading-[1.4615] tracking-normal text-[#FCF6F5] shadow-[0_15px_15px_-15px_rgba(255,159,142,1)] hover:bg-[#F98F75] hover:text-[#FCF6F5]",
+          className
+        )
+      }
     >
       {children}
     </button>
