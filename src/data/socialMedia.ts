264bb35fdd30ae33a89e97efc29a11c40a7a06ff
unknown <flucasjs@gmail.com>
2024-02-26 03:07:10 -0800

    Add social media data

---
+++ src/data/socialMedia.ts
@@ -0,0 +1,35 @@
+import facebookIcon from '@/assets/icons/icon-facebook.svg';
+import twitterIcon from '@/assets/icons/icon-twitter.svg';
+import pinterestIcon from '@/assets/icons/icon-pinterest.svg';
+import instagramIcon from '@/assets/icons/icon-instagram.svg';
+import youtubeIcon from '@/assets/icons/icon-youtube.svg';
+
+const socialMedia = [
+  {
+    name: 'Facebook',
+    icon: facebookIcon,
+    link: '#',
+  },
+  {
+    name: 'Twitter',
+    icon: twitterIcon,
+    link: '#',
+  },
+  {
+    name: 'Pinterest',
+    icon: pinterestIcon,
+    link: '#',
+  },
+  {
+    name: 'Instagram',
+    icon: instagramIcon,
+    link: '#',
+  },
+  {
+    name: 'Youtube',
+    icon: youtubeIcon,
+    link: '#',
+  },
+]
+
+export default socialMedia;
